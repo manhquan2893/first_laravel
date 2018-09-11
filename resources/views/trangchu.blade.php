b55122509f162e7704aa5fa2e29403d2242d8579
Your Git's Name <Your Email>
2018-09-11 21:14:45 +0700

    dsdfdf

---
+++ resources/views/trangchu.blade.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html lang="en">
+        <head>
+            <!-- Required meta tags -->
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+                <!-- Bootstrap CSS -->
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+                <link rel="stylesheet" href="{{asset('css/layout.css')}}">
+
+                <title>Hello, world!</title>
+        </head>
+        <body>
+               <!-- start container -->
+               <div class="container">
+                    <div class="header">
+                            <a href="">
+                                <h3>TOANMATH.COM</h3>
+                               <hr>
+                               <h6 >Tải tài liệu và đề thi môn Toán miễn phí</h6>
+                            </a>
+                    </div>
+               </div>
+               <!-- start menu -->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="container">
+
+                        <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a href=""><i class="fas fa-home"></i></a></li>
+                       <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Toán
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="#">Toán 10</a>
+                          <a class="dropdown-item" href="#">Toán 11</a>
+                          <a class="dropdown-item" href="#">Toán 12</a>
+                        </div>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                      </li>
+                      <li class="nav-item dropdown mr-auto">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                      </li>
+                      <li class="nav-item ">
+                          <form class="form-inline my-2 my-lg-0">
+                          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                      </li>
+                    </ul>
+
+                    </div>
+
+                     </div>
+                </nav>
+                <!-- main-content -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 widget">
+                            <div class="widget-title">
+                                <hr style="background: red; font-size: 3px;margin-bottom: -15px;">
+                                <span style="background: rgb(255, 255, 221);">Bài viết mới nhất</span>
+                            </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                        </div>
+                        <div class="col-md-4 widget">
+                            <div class="widget-title">
+                                <hr style="background: red; font-size: 3px;margin-bottom: -15px;">
+                                <span style="background: rgb(255, 255, 221);">Bài viết mới nhất</span>
+                            </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                        </div>
+                        <div class="col-md-4 widget">
+                            <div class="widget-title">
+                                <hr style="background: red; font-size: 3px;margin-bottom: -15px;">
+                                <span style="background: rgb(255, 255, 221);">Bài viết mới nhất</span>
+                            </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                                <div class="media">
+                                  <img class="mr-3" src="https://via.placeholder.com/80x80" alt="Generic placeholder image">
+                                  <div class="media-body">
+
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                                  </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Optional JavaScript -->
+                <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+        </body>
+</html>
